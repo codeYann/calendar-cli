3e934e986689b7cb178070e3b7123756bee29401
codeYann <ccyan05@alu.ufc.br>
2023-01-31 08:42:37 -0300

    Setting absolute path to credentials.json file

--- cli/api/auth.py
+++ cli/api/auth.py
@@ -1,6 +1,5 @@
 from typing import List
 from google_auth_oauthlib.flow import InstalledAppFlow
-import os.path
 
 SCOPES: List[str] = [
     "https://www.googleapis.com/auth/calendar",
@@ -10,25 +9,12 @@ SCOPES: List[str] = [
 ]
 
 
-# class BaseAuth:
-#     def __init__(
-#         self, scopes: List[str] = SCOPES, path: str = "../../credentials.json"
-#     ) -> None:
-#         dir_name: str = os.path.dirname(__file__)
-#         file_name: str = os.path.join(dir_name, path)
-#         flow: InstalledAppFlow = InstalledAppFlow.from_client_secrets_file(
-#             file_name, scopes=scopes
-#         )
-#         flow.run_local_server()
-#         self.file_path = file_name
-#         self.credentials = flow.credentials
-
-
 class BaseAuth:
     def __init__(self, scopes: List[str]) -> None:
-        base_dir = os.path.dirname(os.path.abspath(__file__))
-        file_name = os.path.join(base_dir, "../../credentials.json")
-        # flow = InstalledAppFlow.from_client_secrets_file(file_name, scopes=scopes)
-        # flow.run_local_server()
-        self.file_path = file_name
-        # self.credentials = flow.credentials
+        file_name = "/home/codeyan/projects/calendar-cli/credentials.json"
+        flow = InstalledAppFlow.from_client_secrets_file(
+            file_name,
+            scopes=scopes
+        )
+        flow.run_local_server()
+        self.credentials = flow.credentials
