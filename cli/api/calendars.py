e3f630f9045535e8f8ca9839b5dc1b6df3c600d4
codeYann <ccyan05@alu.ufc.br>
2023-01-31 08:45:28 -0300

    Refactoring calendar.py

--- cli/api/calendars.py
+++ cli/api/calendars.py
@@ -1,20 +1,18 @@
-# from googleapiclient.discovery import build
-
-# from .auth import BaseAuth, SCOPES
+from googleapiclient.discovery import build
 from auth import BaseAuth, SCOPES
 
 
-class CalendarApi(BaseAuth):
+class CalendarAPI(BaseAuth):
     def __init__(self, calendar_id: str) -> None:
         super().__init__(scopes=SCOPES)
         self.calendar_id = calendar_id
 
     def get_calendar(self) -> None:
-        print(self.file_path)
-        # with build("calendar", "v3") as service:
-        # calendar = service.calendars().get(calendarId=self.calendar_id).execute()
-        # print(calendar)
-
-
-tst = CalendarApi("codeyan10@gmail.com")
-tst.get_calendar()
+        with build("calendar", "v3", credentials=self.credentials) as service:
+            calendar = (
+                service
+                .calendars()
+                .get(calendarId=self.calendar_id)
+                .execute()
+            )
+            print(calendar)
